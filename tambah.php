cf62c0720e200d7acc2bb452efa637a04a46c828
ini_orang <freemiumlemper300@gmail.com>
2022-11-21 10:59:01 +0700

    Update tambah.php

--- tambah.php
+++ tambah.php
@@ -9,46 +9,68 @@
     <script src='main.js'></script>
 </head>
 <body>
-    <form action="" method="post">
+    <form action="tambah.php" method="post">
         <p>
             <label for="Tahun">Tahun kendaraan</label><br>
-            <input type="number" name="tahun_form">
+            <input type="number" name="tahun_form" required>
         </p>
         <p>
             <label for="Merk">Merek kendaraan</label><br>
-            <input type="text" name="merek_form">
+            <input type="text" name="merek_form" required>
         </p>
         <p>
             <label for="Model">Model kendaraan</label><br>
-            <input type="text" name="model_form">
+            <input type="text" name="model_form" required>
         </p>
             <label for="Transmission">Transmission</label><br>
-            <input type="radio" name="transmission" value="automatic">Automatic
-            <input type="radio" name="transmission" value="manual">Manual
+            <input type="radio" name="transmission" value="Automatic" required>Automatic
+            <input type="radio" name="transmission" value="Manual">Manual
         <p>
             <label for="Plat">Plat Nomer kendaraan</label><br>
-            <input type="number" name="plat_form">
+            <input type="text" name="plat_form" required>
         </p>
         <p>
             <label for="Interior">Nilai kondisi interior</label><br>
-            <input type="number" name="interior_form">
+            <input type="number" name="interior_form" required>
         </p>
         <p>
             <label for="Mesin">Nilai kondisi mesin</label><br>
-            <input type="number" name="mesin_form">
+            <input type="number" name="mesin_form" required>
         </p>
         <p>
             <label for="Exterior">Nilai kondisi exterior</label><br>
-            <input type="number" name="exterior_form">
+            <input type="number" name="exterior_form" required>
         </p>
         <p>
             <label for="Harga">Harga awal</label><br>
-            <input type="number" name="harga_form">
+            <input type="number" name="harga_form" required>
         </p>
+        <button type="submit" name="Submit">Simpan</button>
     </form>
-    <button name="Submit">Simpan</button>
     <a href="Dashboard.php">
         <button>Batalkan</button>
     </a>
+    <?php
+    if(isset($_POST['Submit'])){
+        $thn = $_POST['tahun_form'];
+        $merek = $_POST['merek_form'];
+        $model = $_POST['model_form'];
+        $trans = $_POST['transmission'];
+        $plat = $_POST['plat_form'];
+        $inter = $_POST['interior_form'];
+        $mesin = $_POST['mesin_form'];
+        $exterior = $_POST['exterior_form'];
+        $harga = $_POST['harga_form'];
+
+        include_once("config.php");
+        $list = mysqli_query($mysqli,"INSERT INTO kendaraan(tahun_kendaraan,merek_kendaraan,
+        model_kendaraan,transmisi_kendaraan,plat_no_kendaraan,nilai_kondisi_interior,nilai_kondisi_mesin,
+        nilai_kondisi_exterior,harga_awal) VALUES('$thn','$merek','$model','$trans','$plat','$inter',
+        '$mesin','$exterior','$harga')");
+
+        header("Location:Dashboard.php");
+        
+    }
+    ?>
 </body>
 </html>
